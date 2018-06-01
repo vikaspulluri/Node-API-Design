27b5c9f23f48bde79d4a5fdf3fd9b29f89faa14c
vikaspulluri <vikasiiitn@gmail.com>
2018-06-01 23:39:29 +0530

    Configuration

---
+++ assignment-six/server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var api = require('./api/api');
+var err = require('./middleware/errorMiddleware');
+// setup the app middlware
+require('./middleware/appMiddlware')(app);
+
+// setup the api
+app.use('/api/', api);
+// set up global error handling
+
+app.use(err());
+// export the app for testing
+module.exports = app;
