aac5f6b17f9d6d455d5d0a15722aac836a97da2c
vikaspulluri <vikasiiitn@gmail.com>
2018-07-18 00:20:44 +0530

    assignment-9

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -1,20 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//It's the keyword 'type' that distiguishes nested object and string
-var UserSchema = new Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: true
-	},
 
-	address:{
-		state: String,
-	}
-	/* Avoid type keyword. If you want to use, use like below
-	type:{
-		type: String
-	}*/
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('user', UserSchema);
